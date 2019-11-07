c15b6bbcbb8bdd0345dd86622c787541512222c3
Natbussakon <natbussakon@gmail.com>
2019-11-07 13:46:59 +0700

    แก้เมธอดเสร็จแล้ว

--- src/net/bkkgp/web/tire.java
+++ src/net/bkkgp/web/tire.java
@@ -16,19 +16,17 @@ public class tire extends HttpServlet {
 
         response.setCharacterEncoding("UTF-8");
         HttpSession session = request.getSession(false);
-        String email = session.getAttribute("email").toString();
-        session.setAttribute("email",email);
         try (PrintWriter out = response.getWriter()) {
 
-            String roll = request.getParameter("keyword");
+            String roll = request.getParameter("brand");
             System.out.println(roll);
             DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-            String dbURL = "jdbc:mysql://167.71.211.97:3306/rb_2";
-            String dbUser = "roadbu";
-            String dbPass = "roadbuddyadmin1";
+            String dbURL = "jdbc:mysql://167.99.76.137:3306/BKK GROUP";
+            String dbUser = "bkkgroup";
+            String dbPass = "212224236248";
             Connection con = DriverManager.getConnection(dbURL, dbUser, dbPass);
 
-            PreparedStatement ps = con.prepareStatement("select * from rb_2.place where too like  ?");
+            PreparedStatement ps = con.prepareStatement("select * from tire where Product_ID like  ?");
             String forSql = "%" + roll + "%";
             ps.setString(1, forSql);
 
@@ -39,63 +37,73 @@ public class tire extends HttpServlet {
             /* Printing column names */
             ResultSetMetaData rsmd = rs.getMetaData();
 
-            out.print("<html>");
-            out.print("<head>");
-            out.print("<title>คุณสมบัติ</title>");
-            out.print("<link rel=\"stylesheet\" href=\"css_tire_attribute.css\">");
-            out.print("</head>");
-            out.print("<body>");
-            out.print("<div class=\"pageBox\">");
-            out.print("<i class=\"fa fa-bars menu\" aria-hidden=\"true\"></i>");
-            out.print("</div>");
-            out.print("<ul>");
-            out.print("<li><a href=\"home.jsp\">Home</a></li>");
-            out.print("<li><a href=\"${pageContext.request.contextPath}/profileprocess\">Profile</a>");
-            out.print("<li><a href=\"chat.jsp\">Chat</a>");
-            out.print("<li><a href=\"notification.jsp\">Notifications</a></li>");
-            out.print("<li><a href=\"${pageContext.request.contextPath}/logoutprocess\">Log out</a>");
-            out.print("</ul>");
-            out.print("</nav>");
-            out.print("<div class=\"pageBox\">");
-            out.print("<h1 align=\"center\">Result</h1><h2 align=\"center\" >\""+roll+"\"</h2>");
+
+
 
             /* Printing result */
             while (rs.next()) {
                 //out.print("<tr><td>" + rs.getString(2) + "</td><td>" + rs.getString(3) + "</td><td>" + rs.getString(4) + "</td><td>" + rs.getString(5) + "</td><td>" + rs.getString(6) + "</td><td>" + rs.getString(7) +"</td><td>" + rs.getString(8) +"</td><td>" + rs.getString(9) +"</td><td>" + rs.getString(10) +"</td></tr>");
-                out.print("<div class=\"resultBox1\" align=\"center\">");
-                out.print("<table width=\"100%\" >");
-                out.print("<tr>");
-                out.print("<td>");
-                out.print("<center><img src=\""+rs.getString("pic")+"\" class=\"profile-img\"/></center>");
-                out.print("</td>");
-                out.print("<td>");
-                out.print("<h3>"+rs.getString("name")+" "+rs.getString("lname")+"</h3>");
+                out.print("<html>");
+                out.print("<head>");
+                out.print("<title>คุณสมบัติ</title>");
+                out.print("<link rel=\"stylesheet\" href=\"css_tire_attribute.css\">");
+                out.print("</head>");
+                out.print("<body>");
+                out.print("<div class=\"pageBox\">");
+                out.print("<div class=\"pageH\" align=\"right\"> <a href=\"tire.jsp\"><button class=\"buttonBack\" style=\"vertical-align:middle\"><span>BACK </span> </button></a>");
+                out.print("</div>");
+                out.print("<div class=\"pageGen\" style=\"overflow-y:hidden;height:420px\">");
                 out.print("<table>");
-                out.print("<tr>");
-                out.print("<td><p>Status </p></td><td><p>Driver</p></td>");
+                out.print(" <tr>");
+                out.print("<td width=\"500px\">");
+                out.print("<img src=\"./img/22_MICHELINPilot%20Super%20Sport1%204.png\" height=\"350\"/>");
+                out.print("</td>");
+                out.print("<td width=\"500px\">");
+                out.print("<table bgcolor=\"silver\" >");
+                out.print("<td class=\"tableBorder\" width=\"450\">ความกว้างหน้ายาง (มม.)</td> <td bgcolor=\"#eee8aa\" width=\"250\">"+rs.getString("Tire_width")+"</td>");
                 out.print("</tr>");
                 out.print("<tr>");
-                out.print("<td><p>From</p></td><td><p>"+rs.getString("from")+"</p></td>");
+                out.print("<td class=\"tableBorder\">ความสูงของแก้มยาง (ซีรี่ย์,เปอร์เซนต์)</td> <td bgcolor=\"#eee8aa\">"+rs.getString("Height_of_the_sidewall(series)")+"</td>");
                 out.print("</tr>");
                 out.print("<tr>");
-                out.print("<td> <p>To</p></td><td><p>"+rs.getString("too")+"</p></td>");
+                out.print("<td class=\"tableBorder\">เส้นผ่านศูนย์กลางกระทะล้อ (นิ้ว)</td> <td bgcolor=\"#eee8aa\">"+rs.getString("Wheel_d")+"</td>");
                 out.print("</tr>");
                 out.print("<tr>");
-                out.print("<td><p>Time</p></td><td><p>"+rs.getString("time")+":"+rs.getString("minute")+"</p></td>");
+                out.print("<td class=\"tableBorder\" >ความสูงแก้มยาง(มม.)</td> <td bgcolor=\"#eee8aa\" >5g55</td>");
                 out.print("</tr>");
                 out.print("<tr>");
-                out.print("<td><p>Date</p></td><td><p>"+rs.getString("date")+" "+rs.getString("month")+" "+rs.getString("year")+"</p></td>");
+                out.print("<td class=\"tableBorder\">ความสูงของยางโดยรวม(มม.)</td> <td bgcolor=\"#eee8aa\">5g55</td>");
+                out.print("</tr>");
+                out.print("<tr>");
+                out.print("<td class=\"tableBorder\">ความเร็วสูงสุด (กม./ชม.)</td> <td bgcolor=\"#eee8aa\">5g55</td>");
+                out.print("</tr>");
+                out.print("<tr>");
+                out.print("<td class=\"tableBorder\">รับน้ำหนักต่อล้อ (กก.)</td> <td bgcolor=\"#eee8aa\">5g55</td>");
+                out.print("</tr>");
+                out.print("<tr>");
+                out.print("<td class=\"tableBorder\">ความกว้างของล้อที่ใช้ทดแทนกันได้ (นิ้ว)</td> <td bgcolor=\"#eee8aa\">5g55</td>");
+                out.print("</tr>");
+                out.print("<tr>");
+                out.print("<td class=\"tableBorder\">ความดันลมยางสูงสุดต่อล้อ (ปอนด์/ตร.นิ้ว)</td> <td bgcolor=\"#eee8aa\">5g55</td>");
+                out.print("</tr>");
+                out.print("<tr>");
+                out.print("<td class=\"tableBorder\">ประเภทยาง</td> <td bgcolor=\"#eee8aa\">5g55</td>");
+                out.print("</tr>");
+                out.print("<tr>");
+                out.print("<td class=\"tableBorder\">ราคา (ต่อเส้น)</td> <td  bgcolor=\"#e9967a\">5g55</td>");
+                out.print("</tr>");
+                out.print("<tr>");
+                out.print("<td >จำนวนคลังสินค้า</td> <td  bgcolor=\"#8fbc8f\">5g55</td>");
                 out.print("</tr>");
                 out.print("</table>");
                 out.print("</td>");
                 out.print("</tr>");
                 out.print("</table>");
                 out.print("</div>");
-
-
+                out.print("</div>");
+                out.print("</body>");
+                out.print("</html>");
             }
-            out.print("</body>");
-            out.print("</html>");
 
         } catch (Exception e2) {
             e2.printStackTrace();
