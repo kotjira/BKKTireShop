7f4fa4a051e8f8819a16dda44218f9b57bce937b
Natbussakon <natbussakon@gmail.com>
2019-11-12 19:00:13 +0700

    กำลังเพิ่มหน้าลูกค้า

---
+++ src/net/bkkgp/model/Customerr.java
@@ -0,0 +1,129 @@
+package net.bkkgp.model;
+
+import java.io.Serializable;
+
+/**
+ * JavaBean class used in jsp action tags.
+ * @author Ramesh Fadatare
+ */
+public class Customerr implements Serializable {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    private String firstName;
+    private String lastName;
+    private String birthDay;
+    private String birthMonth;
+    private String birthYear;
+    private String sex;
+    private String nationality;
+    private String homeID;
+    private String post;
+    private String phoneMe;
+    private String email;
+    private String education;
+    private String Position;
+    private String userName;
+    private String password1;
+    private String password2;
+
+    public String getFirstName() {
+
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getBirthDay() {
+        return birthDay;
+    }
+    public void setBirthDay(String birthDay) {
+        this.birthDay = birthDay;
+    }
+    public String getBirthMonth() {
+        return birthMonth;
+    }
+    public void setBirthMonth(String birthMonth) {
+        this.birthMonth = birthMonth;
+    }
+    public String getBirthYear() {
+        return birthYear;
+    }
+    public void setBirthYear(String birthYear) {
+        this.birthYear = birthYear;
+    }
+    public String getSex() {
+        return sex;
+    }
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+    public String getNationality() {
+        return nationality;
+    }
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+    public String getHomeID() {
+        return homeID;
+    }
+    public void setHomeID(String homeID) {
+        this.homeID = homeID;
+    }
+    public String getPost() {
+        return post;
+    }
+    public void setPost(String post) {
+        this.post = post;
+    }
+    public String getPhoneMe() {
+        return phoneMe;
+    }
+    public void setPhoneMe(String phoneMe) {
+        this.phoneMe = phoneMe;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getEducation() {
+        return education;
+    }
+    public void setEducation(String education) {
+        this.education = education;
+    }
+    public String getPosition() {
+        return Position;
+    }
+    public void setPosition(String Position) {
+        this.Position = Position;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getPassword1() {
+        return password1;
+    }
+    public void setPassword1(String password1) {
+        this.password1 = password1;
+    }
+    public String getPassword2() {
+        return password1;
+    }
+    public void setPassword2(String password2) {
+        this.password2 = password2;
+    }
+}
